07addef892cdcc81af692dd7fa67ff1c920d7a63
Sai Prasanth <Sai Prasanth@LAPTOP-SSRQBIMK>
2024-10-06 23:54:04 +0530

    added another example for switch

---
+++ CoreJavaDemo/src/test/java/com/corejavademo/SwitchCase2.java
@@ -0,0 +1,29 @@
+package com.corejavademo;
+
+import java.util.Scanner;
+
+public class SwitchCase2 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		int x=10;
+		int y=10;
+		
+		System.out.println("what is the value for 10+10");
+		int xy=sc.nextInt();
+		
+		switch(xy){
+			
+		case 20:
+			System.out.println("yes it is 20---correct answer");
+			break;
+			
+		default:
+			System.out.println("it is wrong answer");
+		}
+		sc.close();
+
+	}
+
+}
